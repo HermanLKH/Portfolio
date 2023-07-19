29bb25465728ff21fbf3de47602fbf99922e27e4
HermanLKH <57675016+HermanLKH@users.noreply.github.com>
2023-07-19 19:05:04 +0800

    Add files via upload

---
+++ ContactsManager/ContactsManager.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,40 @@
+﻿using DbContext;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace CRUDTests
+{
+	// execute(copy) everything in Program.cs
+	public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+	{
+		// override configuration of Program.cs which handles http requests
+		protected override void ConfigureWebHost(IWebHostBuilder builder)
+		{
+			base.ConfigureWebHost(builder);
+
+			// change to testing environment
+			builder.UseEnvironment("Test");
+
+			// change from real database to in-memory database
+			builder.ConfigureServices(services =>
+			{
+				// descripter => represent service & its lifetime
+				var descripter = services.SingleOrDefault(temp => temp.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+				// remove the real dbcontext service
+				if(descripter != null)
+				{
+					services.Remove(descripter);
+				}
+				// add in-memory database service
+				services.AddDbContext<ApplicationDbContext>(options =>
+				{
+					options.UseInMemoryDatabase("DatabaseForTesting");
+				});
+			});
+		}
+	}
+}
---
+++ ContactsManager/ContactsManager.IntegrationTests/PersonsControllerIntegrationTest.cs
@@ -0,0 +1,40 @@
+﻿using AutoFixture;
+using Fizzler.Systems.HtmlAgilityPack;
+using FluentAssertions;
+using HtmlAgilityPack;
+
+namespace CRUDTests
+{
+	// integration testing/ blackbox testing
+	public class PersonsControllerIntegrationTest : IClassFixture<CustomWebApplicationFactory>
+	{
+		// http client to simulate a browser request
+		private readonly HttpClient _client;
+
+		// inject the factory created and use that to create a http client
+		public PersonsControllerIntegrationTest(CustomWebApplicationFactory factory)
+		{
+			_client = factory.CreateClient();
+		}
+
+		#region Index
+		[Fact]
+		public async Task Index_ToReturnView()
+		{
+			// act
+			HttpResponseMessage response = await _client.GetAsync("Persons/Index");
+
+			// assert
+			response.Should().BeSuccessful();
+
+			string responseBody = await response.Content.ReadAsStringAsync();
+			HtmlDocument html = new HtmlDocument();
+			// generate html dom based on response body
+			html.LoadHtml(responseBody);
+
+			var document = html.DocumentNode;
+			document.QuerySelectorAll("table.persons").Should().NotBeNull();
+		}
+		#endregion
+	}
+}
